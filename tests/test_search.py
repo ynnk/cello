aaa7b37427ec799e4f815c93265c0175ff8c9dce
Emmanuel N <enavarro222@gmail.com>
2013-10-16 13:32:24 +0200

    Add a first exemple test case

---
+++ tests/test_search.py
@@ -0,0 +1,12 @@
+#-*- coding:utf-8 -*-
+import unittest
+from cello.search.base_search import AbstractSearch
+
+class TestAbstractSearch(unittest.TestCase):
+    def setUp(self):
+        self.abstract_search = AbstractSearch("test_search")
+
+    def test_call(self):
+        self.assertRaises(NotImplementedError, self.abstract_search, "test query")
+
+
