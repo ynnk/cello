fa504b2002c9fc696083572247c248ea1e00e88d
ynnk <ychudy@gmail.com>
2014-01-24 12:09:37 +0100

    annotations envarro should read it

--- cello/optionable.py
+++ cello/optionable.py
@@ -20,6 +20,22 @@ Class
 from collections import OrderedDict
 from cello.utils import parse_bool
 
+# ynnk
+# AbstractOption : why do we need an Abstract where there is a Generic that can be extended
+# parse_options :should parse only not be a setter
+# default: why setting default also set the value
+# set()  :setting value with set is unnecessary the property covers use case perfectly
+# cast/parse : cast is an attribute function parse is part of interface
+#         should use cast only or call it parse  but remove one from interface
+# set_from_str : if options are given as str , other way are
+#           >>> option.set(string, parse=True)
+# validate : we should be able to override validate from __init__
+#    instead of writting a new class to override validate.
+#    validate raises exception catch by caller         
+#    ex: we want an int in range [0:5] 
+#           >>> Option("name", "desc", parse=lambda x : int(x), validate=lamdba x : O if x < 0 else min(4,x) )
+# or create a RangeOption ?
+# first case is fast an easy way but fr reusability 
 
 class AbstractOption(object):
     """ Abstract Option, should not be use directly
@@ -339,7 +355,7 @@ class Optionable(object):
     def set_option_value(self, opt_name, value, from_str=False):
         """ Set tthe value of one option.
         
-        :param opt_name: option name
+        :   param opt_name: option name
         :type opt_name: str
         :param value: the new value
         """
