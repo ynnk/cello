1f436bb6ce4b19c2564d9047e956c4350c76f951
Emmanuel N <enavarro222@gmail.com>
2014-03-22 17:38:05 +0100

    start moving layout in a proper module

---
+++ cello/layout/__init__.py
@@ -0,0 +1,14 @@
+#-*- coding:utf-8 -*-
+""" :mod:`cello.layout`
+======================
+
+.. toctree::
+
+    cello.layout.transform
+
+"""
+import logging
+
+_logger = logging.getLogger("cello.graphs.layout")
+
+
