9d531902b6b3ffa5008827efd38e558df9798dfc
Emmanuel N <enavarro222@gmail.com>
2014-03-26 10:21:11 +0100

    mv random_vertex to cello.graph

--- cello/graphs/__init__.py
+++ cello/graphs/__init__.py
@@ -9,7 +9,24 @@ from cello.pipeline import Optionable
 
 EDGE_WEIGHT_ATTR = "weight"
 
+def random_vertex(graph, attr=None, from_edges=False):
+    """ return a random vertex of the given graph
 
+    :param attr: if not None return the attribute 'attr' of the random vertex, instead of the id (of the random vertex).
+    :param from_edges: if True get an edges by random and then pick one of the ends of the edge by random
+    """
+    if from_edges:
+        # random edge
+        es = random.choice(graph.es)
+        vid = random.choice([es.source, es.target])
+    else:
+        # random node
+        vid = random.choice(xrange(self.graph.vcount()))
+    # return attr or vid
+    if attr is not None:
+        return self.graph.vs[vid][attr]
+    else:
+        return vid
 
 class GraphBuilder(object):
     """ Abstract class to build a igraph graph object by parsing a source.
