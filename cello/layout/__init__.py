83c978ccdc5332b172bad1af9b63f17132ed7d19
ynnk <ychudy@gmail.com>
2014-04-29 10:14:39 +0200

    imports

--- cello/layout/__init__.py
+++ cello/layout/__init__.py
@@ -15,25 +15,15 @@ SubModules
 Helpers
 -------
 """
-import igraph as ig
-
-from cello.layout.simple import KamadaKawaiLayout
-from cello.layout.simple import GridLayout
-from cello.layout.simple import RandomLayout
-from cello.layout.simple import FruchtermanReingoldLayout
-
-from cello.layout.prox import ProxLayoutPCA
-from cello.layout.prox import ProxLayoutRandomProj
-from cello.layout.prox import ProxBigraphLayoutPCA
-from cello.layout.prox import ProxBigraphLayoutRandomProj
 
 
 def export_layout(layout):
     """ Build a dictionary view of a layout
 
+    >>> import igraph as ig
     >>> graph = ig.Graph.Formula("a--b--c--d, a--a")
     
-    >>> from cello.layout import GridLayout
+    >>> from cello.layout.simple import GridLayout
     >>> layout_builder = GridLayout(dim=2)
     >>> layout = layout_builder(graph)
     >>> export_layout(layout)
