a99f7812a4955e9733101324f75ca4455c33c9ae
ynnk <ychudy@gmail.com>
2014-05-04 16:03:08 +0200

    typos

--- cello/expanders/__init__.py
+++ cello/expanders/__init__.py
@@ -21,7 +21,7 @@ Abstract & Noop expanders
 from cello.pipeline import Optionable
 
 class AbstractExpand(Optionable):
-    """ Expand a list of L{KodexDoc}.
+    """ Expand a list of L{Doc}.
     This is an abstract class, the method
     """
     def __init__(self, name):
