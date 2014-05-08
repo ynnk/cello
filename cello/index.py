347bf8b74f791e28a5a2a9b0af423e25682dc69f
Emmanuel N <enavarro222@gmail.com>
2014-05-08 16:07:25 +0200

    pylint + doc + cosmetic

--- cello/index.py
+++ cello/index.py
@@ -1,15 +1,19 @@
 #-*- coding:utf-8 -*-
+""" :mod:`cello.index`
+======================
 
+"""
+#TODO: add doc
 import warnings
-import logging
-import os
 
-from cello.schema import Doc
-from cello.exceptions import  CelloError
+from cello.exceptions import CelloError
+from cello.schema import Doc    #FIXME: unused import
 
 
 class CelloIndexError(CelloError):
     """Raised when index cannot be open """
+    pass
+
 
 class Index:
     """ Abstract class, provide methods to access an index of a collection.
@@ -19,52 +23,49 @@ class Index:
 
     def __len__(self):
         """ Number of documents in the index
-        @return: number of document in the index
-        @rtype: L{int}
+        
+        :return: number of document in the index
+        :rtype: `int`
         """
         raise NotImplementedError
 
     def has_document(self, docnum):
         """ Whether a document is in the index
         
-        @param docnum: the document uniq identifier (in the collection).
-        @type docnum: L{str} or L{unicode}
+        :param docnum: the document uniq identifier (in the collection).
+        :type docnum: `str` or `unicode`
         
-        @return: True if the document is present in the index
-        @rtype: boolean
+        :return: True if the document is present in the index
+        :rtype: boolean
         """
         raise NotImplementedError
 
     def get_document(self, docnum):
         """ Retrun a document
         
-        @note: it is preferable to use L{get_documents} when you have multiple
+        @note: it is preferable to use :func:`get_documents` when you have multiple
         documents tio fetch, implementations are often more efficent.
         
-        @see: L{get_documents}
-        
-        @param docnum: the document uniq identifier (in the collection).
-        @type docnum: str or unicode
-        @return: a L{KodexDoc} object with at least a setted docnum.
-        @rtype: L{KodexDoc}
+        :param docnum: the document uniq identifier (in the collection).
+        :type docnum: str or unicode
+        :return: a :class:`Doc` object with at least a setted docnum.
+        :rtype: :class:`Doc`
         """
         raise NotImplementedError
 
     def get_documents(self, docnums):
         """ Seek some documents from the index
         
-        @note: it is preferable to use this method raither the L{get_document}
-        when you have multiple documents io fetch, many implementations are
-        more efficent.
+        @note: it is preferable to use this method raither the 
+        :func:`get_documents` when you have multiple documents io fetch, many
+        implementations are more efficent.
         
-        @see: L{get_document}
-        
-        @param docnums: the list (or at least iterable) of document's uniq
+        :param docnums: the list (or at least iterable) of document's uniq
         identifier (in the collection).
-        @type docnums: list of str or unicode
-        @return: a generator (or a list) of L{KodexDoc} object with at least
+        :type docnums: list of str or unicode
+        :return: a generator (or a list) of :class:`Doc` object with at least
         a setted docnum.
-        @rtype: (L{KodexDoc}, ...)
+        :rtype: (:class:`Doc`, ...)
         """
         warnings.warn("Unefficient implementation: it calls self.get_document for each doc", RuntimeWarning)
         return (self.get_document(docnum) for docnum in docnums)
@@ -77,7 +78,7 @@ class Index:
     def add_document(self, kdoc):
         """ Add a document in the index
         @param kdoc: the document to add
-        @type kdoc: L{KodexDoc}
+        @type kdoc: :class:`Doc`
         @return: True if the document has been added correctly
         @rtype: boolean
         """
@@ -86,7 +87,7 @@ class Index:
     def add_documents(self, kdocs):
         """ Add a set of documents in the index
         @param kdocs: a list of  document to add
-        @type kdocs: (L{KodexDoc}, ...)
+        @type kdocs: (:class:`Doc`, ...)
         @return: list of docnum that has not been added correctly
         @rtype: [int, ...]
         """
--- cello/providers/solr.py
+++ cello/providers/solr.py
@@ -1,43 +1,52 @@
 #-*- coding:utf-8 -*-
+""" :mod:`cello.providers.solr`
+==================================
 
-import logging
-import os, os.path
+Set of class to acces a SOLR server
+"""
+
+import os
+import os.path
 
 # solr api required
-try :
+try:
     import sunburnt
-except :
+except:
     print("Error importing sunburnt library for Apache Solr, run $ sudo pip install sunburnt")
+    raise
 
 from cello.schema import Doc
 from cello.pipeline import Optionable
+
+#FIXME: unused import
 from cello.index import Index, CelloIndexError
 from cello.search import AbstractSearch
 from cello.writers import AbstractWriter
 
 
 class SolrIndex(Index):
-    """ Access an L{Index} stored in a solr db .
-        Early version !
-        * get solr index
-        >>> idx = SolrIndex(idx='yours')
-        
-        * get document count
-        >>> len(idx)
-        
-        * get field names declared in schema
-        >>> idx.fields()
-        
-        * iter over docnums
-        >>> idx.iter_docnums()
-        
-        * iter over all docnument in index
-        >>> idx.get_document(idx.iter_docnums())
+    """ Access an solr index.
+
+    Early version !
+    * get solr index
+
+    Here is an usage exemple:
+
+        idx = SolrIndex(idx='yours')
+        # get document count
+        len(idx)
+        # get field names declared in schema
+        idx.fields()
+        # iter over docnums
+        idx.iter_docnums()
+        # iter over all docnument in index
+        idx.get_document(idx.iter_docnums())
     """
-    # TODO multiple add !    
-    
-    
-    def __init__(self, host="http://localhost:8983/solr/", idx=None , **kwargs):
+    #TODO: ^ trouvé comment en faire des vrai doctest (fake server)
+
+    # TODO multiple add !
+
+    def __init__(self, host="http://localhost:8983/solr/", idx=None, **kwargs):
         """ 
         @param host: base url for connection
         @param idx: solr index name
@@ -45,7 +54,7 @@ class SolrIndex(Index):
             - cache: max document count in each retrieving request
             - fl: field names returned in a document ',' separated ex: "text, url,score"
             - wrap: fct(docdict) function to wrap dict to object such as lambda docdict : kdoc_wrap(docdict)
-        """        
+        """
         Index.__init__(self)
         # FIXME raise error
         assert host is not None
@@ -54,14 +63,15 @@ class SolrIndex(Index):
         self.conn = sunburnt.SolrInterface(host + idx)
         self.uniq_key = self.conn.schema.unique_key 
         # override specific params for optimizations
-        self.cache = kwargs.get("cache", 100 )
-        self.fl = kwargs.get("fl", "*" ) # will return all field of 
+        self.cache = kwargs.get("cache", 100)
+        self.fl = kwargs.get("fl", "*") # will return all field of 
         self.wrap = kwargs.get("wrap", self._default_wrapper())
+        #TODO ^ pourquoi "kwargs" c'est juste des arguments optionelles ?
 
     def _default_wrapper(self):
         uniq_key = self.uniq_key
         def wrap(doc):
-            kdoc = Doc(docnum=doc.pop(uniq_key)) 
+            kdoc = Doc(docnum=doc.pop(uniq_key))
             kdoc.update(doc)
             return kdoc
         return wrap
@@ -78,21 +88,21 @@ class SolrIndex(Index):
 
     def statistics(self):
         return {
-            "ndocs" : len(self)
+            "ndocs": len(self)
         }
 
     def has_document(self, docnum):
         """Test for a document in Index.  Fetchs document and returns True wether exists """
         return self.get_document(docnum) is not None
 
-    def get_document(self, docnum ):
+    def get_document(self, docnum):
         """ fetch a document given a docnum 
         it will match the given value in field specified as 'uniqueKey' in schema.xml """
         kdoc = None
-        params = {'q': "%s:%s"%(self.uniq_key, docnum),'fl':self.fl,'rows':1}
+        params = {'q': "%s:%s"%(self.uniq_key, docnum), 'fl':self.fl, 'rows':1}
         result = self.conn.search(**params).result
         
-        if result.numFound > 0:      
+        if result.numFound > 0:
             for doc in result.docs:
                 kdoc = self.wrap(doc)
         return kdoc
@@ -105,15 +115,15 @@ class SolrIndex(Index):
         uniq_key = self.uniq_key
         dnum_cache = []
         ccount = 0
-        params = {'fl':self.fl,'start':0,'rows':self.cache}
+        params = {'fl':self.fl, 'start':0, 'rows':self.cache}
         for d in docnums:
             dnum_cache.append(d)
-            ccount +=1
+            ccount += 1
             if ccount == self.cache:
                 solrq = Q()
                 for dnum in dnum_cache:
                     solrq |= Q(**{uniq_key:dnum})
-                params['q']= solrq # lucene query
+                params['q'] = solrq # lucene query
                 result = self.conn.search(**params).result
                 for doc in result.docs:
                     yield wrap(doc)
@@ -123,14 +133,19 @@ class SolrIndex(Index):
     def iter_docnums(self, incr=1000):
         start = 0
         count = len(self)
-        params = {'q': "%s:*"%self.uniq_key,'fl':self.uniq_key,'start':0,'rows':incr}
-        while (start < count ):
+        params = {
+            'q': "%s:*" % self.uniq_key,
+            'fl': self.uniq_key,
+            'start': 0,
+            'rows': incr
+        }
+        while (start < count):
             params['start'] = start
             result = self.conn.search(**params).result
             for doc in result.docs:
                 yield doc[self.uniq_key]
             start += incr
-            
+
     def add_document(self, kdoc):
         raise NotImplementedError
 
@@ -143,40 +158,50 @@ class SolrIndex(Index):
 class SolrSearch(Optionable):
     # FIXME require schema
     def __init__(self, idx):
+        #FIXME; doc needed idx c'est uen SolrIndex ?
         Optionable.__init__(self, name=self.__class__.__name__)
         self._index = idx
-        
-    def __call__(self, query, search_field='*', qf="", fl="*,score",nb_res=10, operator="AND"):
-        """ Perform a search using the Solr webapp       
-        @param search_field: field to search for matching term. If '*' one can set the boosts per field in the @param qf.
-        @param fl: field names returned in a document ',' separated ex: "text, url,score"
-        @param qf : List of fields and the 'boosts' to associate with each of them when building DisjunctionMaxQueries from the user's query. The format supported is fieldOne^2.3 fieldTwo fieldThree^0.4, which indicates that fieldOne has a boost of 2.3, fieldTwo has the default boost, and fieldThree has a boost of 0.4 ... : &qf=
+        #FIXME: add options
+
+    def __call__(self, query, search_field='*', qf="", fl="*,score", nb_res=10, operator="AND"):
+        """ Perform a search using the Solr webapp
+
+        :param search_field: field to search for matching term. If '*' one can set the boosts per field in the @param qf.
+        :param fl: field names returned in a document ',' separated ex: "text, url,score"
+        :param qf : List of fields and the 'boosts' to associate with each of them when building DisjunctionMaxQueries from the user's query. The format supported is fieldOne^2.3 fieldTwo fieldThree^0.4, which indicates that fieldOne has a boost of 2.3, fieldTwo has the default boost, and fieldThree has a boost of 0.4 ... : &qf=
             ths param will be used IF and ONLY  @param search_field is '*'.
-        @param nb_res: max count of document to be returned
-        @param operator: how terms are handled together in a request. possible values ('AND','OR)'
+        :param nb_res: max count of document to be returned
+        :param operator: how terms are handled together in a request. possible values ('AND','OR)'
             ex: query='moteur chat' >> 'chat AND moteur'
         """
         kdocs = []
-        Q = self._index.conn.Q 
+        Q = self._index.conn.Q
         def _query(lucenequery, term):
-            qargs = lambda  term : Q(**{search_field:term}) if search_field != "*" else Q(term) 
-            if len(term)> 0 :
+            #FIXME: doc needed ! que fait cette fonction interne ?
+            qargs = lambda term: Q(**{search_field: term}) if search_field != "*" else Q(term)
+            if len(term) > 0:
                 if operator == "AND":
-                    if term[:1] == "-" : return lucenequery & ~qargs(term) # & not
-                    else : return lucenequery & qargs(term)
+                    if term[:1] == "-":
+                        return lucenequery & ~qargs(term) # & not
+                    else:
+                        return lucenequery & qargs(term)
                 elif operator == "OR":
                     return lucenequery | qargs(term)
-            else : return lucenequery
+            else:
+                return lucenequery
 
         if query:
-            solrq = reduce(_query, query.split(' '), Q()) 
-            params = {'q':solrq,'fl':fl,'rows':nb_res}
-            if search_field == "*" : 
+            solrq = reduce(_query, query.split(' '), Q())
+            params = {'q': solrq, 'fl': fl, 'rows': nb_res}
+            if search_field == "*":
                 # When qf is used &defType=dismax should be set
-                params.update({'defType':'dismax', 'qf':qf})
-            params.update({"debugQuery" : 'on'})
+                params.update({
+                    'defType': 'dismax',
+                    'qf': qf
+                })
+            params.update({"debugQuery": 'on'})
             result = self._index.conn.search(**params).result
-            if result.numFound > 0:      
+            if result.numFound > 0:
                 idx = self._index 
                 for doc in result.docs:
                     kdocs.append(idx.wrap(doc))
