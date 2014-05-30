538b94933e4987da1078b9c67d2108d78c0b61e4
Emmanuel N <enavarro222@gmail.com>
2014-05-30 13:49:33 +0200

    flask api builder now accept multiple inputs

--- cello/utils/web.py
+++ cello/utils/web.py
@@ -8,6 +8,8 @@ helpers to build HTTP/Json Api from cello engines
 import sys
 import json
 
+from collections import OrderedDict
+
 from flask import Flask
 from flask import Blueprint
 from flask import abort, request, jsonify
@@ -26,7 +28,7 @@ class CelloFlaskView(Blueprint):
     """
 
     def __init__(self, engine):
-        """ Build the Blueprint
+        """ Build the Blueprint view over a :class:`.Engine`.
         
         :param engine: the cello engine to serve through an json API
         :type engine: :class:`.Engine`.
@@ -34,47 +36,58 @@ class CelloFlaskView(Blueprint):
         super(CelloFlaskView, self).__init__("cello", __name__)
         self.engine = engine
         # default input
-        self._in_type = None
-        self.set_input_type(Text(vtype=unicode, encoding="utf8"))
+        self._inputs = OrderedDict()
         # default outputs
-        self._outputs = []
+        self._outputs = OrderedDict()
         
         # bind entry points
         self.add_url_rule('/options', 'options', self.options)
-        self.add_url_rule('/play', 'play', self.play,  methods= ["POST", "GET"])
+        self.add_url_rule('/play', 'play', self.play,  methods=["POST", "GET"])
 
     def set_input_type(self, type_or_parse):
-        """ Set the input type
+        """ Set an unique input type.
+        
+        If you use this then you have only one input for the play.
         """
-        if isinstance(type_or_parse, GenericType):
-            self._in_type = type_or_parse
-        elif callable(type_or_parse):
-            self._in_type = GenericType(parse=type_or_parse)
-        else:
+        self._inputs = OrderedDict()
+        default_inputs = self.engine.in_name
+        if len(default_inputs) > 1:
+            raise ValueError("First block of the engine need more than one input, you sould use `add_inpout` for each of them")
+        self.add_input(default_inputs[0], type_or_parse)
+
+    def add_input(self, in_name, type_or_parse):
+        """ declare a possible input
+        """
+        if not isinstance(type_or_parse, GenericType) and callable(type_or_parse):
+            type_or_parse = GenericType(parse=type_or_parse)
+        elif not isinstance(type_or_parse, GenericType):
             raise ValueError("the given 'type_or_parse' is invalid")
+        self._inputs[in_name] = type_or_parse
 
     def add_output(self, out_name, serializer=None):
-        """ add an output
+        """ declare an output
         """
         if serializer is not None and not callable(serializer):
             raise ValueError("the given 'serializer' is invalid")
-        self._outputs.append((out_name, serializer))
+        self._outputs[out_name] = serializer
 
     def options(self):
+        """ Engine options discover HTTP entry point
+        """
         conf = self.engine.as_dict()
-        conf["returns"] = [oname for oname, _ in self._outputs]
+        #TODO add possible inputs
+        conf["returns"] = [oname for oname in self._outputs.iterkeys()]
         return jsonify(conf)
 
     def play(self):
+        """ play HTTP entry point
+        """
         if not request.headers['Content-Type'].startswith('application/json'):
             abort(415) # Unsupported Media Type
         ### get data
         data = request.json
         assert data is not None #FIXME: better error than assertError ?
-        ### check commun errors
-        if not all([inname in data for inname in self.engine.in_name]):
-            #XXX ERROR should be handle
-            raise NotImplementedError()
+        #
         ### parse options
         if "options" in data:
             options = data["options"]
@@ -83,27 +96,43 @@ class CelloFlaskView(Blueprint):
             except ValueError as err:
                 #TODO beter manage input error: indicate what's wrong
                 abort(406)  # Not Acceptable
-        ### parse input (and validate)
-        inputs_data = [data[in_name] for in_name in self.engine.in_name]
-        if len(inputs_data):
-            self._in_type.validate(inputs_data[0])
-        else:
-            raise NotImplementedError("le mutlti input est pas encore géré ici...")
+        ### Check inputs
+        needed_inputs = self.engine.needed_inputs()
+        # check if all needed inputs are possible
+        if not all([inname in self._inputs for inname in needed_inputs]):
+            #XXX this may be check staticly
+            raise NotImplementedError()
+        # check if all inputs are given
+        if not all([inname in data for inname in needed_inputs]):
+            #XXX ERROR should be handle
+            raise NotImplementedError()
+        #
+        ### parse inputs (and validate)
+        inputs_data = {}
+        for inname in needed_inputs:
+            input_val = self._inputs[inname].parse(data[inname])
+            self._inputs[inname].validate(input_val)
+            inputs_data[inname] = input_val
+        #
         ### run the engine
         error = False #by default ok
         try:
-            raw_res = self.engine.play(*inputs_data)
+            raw_res = self.engine.play(**inputs_data)
         except CelloPlayError as err:
             # this is the cello error that we can handle
             error = True
         finally:
             pass
+        #
         ### prepare outputs
         outputs = {}
         results = {}
         if not error:
             # prepare the outputs
-            for out_name, serializer in self._outputs:
+            for out_name, raw_out in raw_res.iteritems():
+                if out_name not in self._outputs:
+                    continue
+                serializer = self._outputs[out_name]
                 # serialise output
                 if serializer is not None:
                     results[out_name] = serializer(raw_res[out_name])
--- tests/test_web.py
+++ tests/test_web.py
@@ -20,7 +20,7 @@ class OptProductEx(Optionable):
         return arg * factor
 
 
-class TestCelloFlaskView(unittest.TestCase):
+class TestCelloFlaskViewSimple(unittest.TestCase):
     maxDiff = None
 
     def setUp(self):
@@ -53,7 +53,7 @@ class TestCelloFlaskView(unittest.TestCase):
 
     def test_play_simple(self):
         # prepare query
-        rdata = {'in': 2}
+        rdata = {'in': '2'}
         json_data = json.dumps(rdata)
         resp = self.app.get('api/play', data=json_data, content_type='application/json')
         # load the results
@@ -67,11 +67,13 @@ class TestCelloFlaskView(unittest.TestCase):
 
     def test_play_fail(self):
         json_data = json.dumps({'in': 10})
+        # max is 5, so validation error
         with self.assertRaises(ValidationError):
             resp = self.app.get('api/play', data=json_data, content_type='application/json')
 
         json_data = json.dumps({'in': "chat"})
-        with self.assertRaises(ValidationError):
+        # parsing error
+        with self.assertRaises(ValueError):
             resp = self.app.get('api/play', data=json_data, content_type='application/json')
 
         json_data = json.dumps({'in': 1})
@@ -101,4 +103,79 @@ class TestCelloFlaskView(unittest.TestCase):
         self.assertTrue("out" in results)
         self.assertEquals(len(results), 1)
         self.assertEquals(results["out"], 2*2*5)
+
+
+class TestCelloFlaskViewMultiInputs(unittest.TestCase):
+    maxDiff = None
+
+    def setUp(self):
+        self.engine = Engine("op1", "op2")
+        self.engine.op1.setup(in_name="in", out_name="middle", required=False)
+        self.engine.op2.setup(in_name="middle", out_name="out")
+
+        self.engine.op1.set(OptProductEx())
+        foisdouze = OptProductEx("foisdouze")
+        foisdouze.force_option_value("factor", 12)
+        self.engine.op2.set(foisdouze, OptProductEx())
+
+        api = CelloFlaskView(self.engine)
+        api.add_input("in", Numeric(vtype=int, min=-5, max=5))
+        api.add_input("middle", Numeric(vtype=int))
+        api.add_output("in")
+        api.add_output("middle")
+        api.add_output("out")
+
+        app = Flask(__name__)
+        app.config['TESTING'] = True
+        app.register_blueprint(api, url_prefix="/api")
+        self.app = app.test_client()
+
+    def test_options(self):
+        resp = self.app.get('api/options')
+        data = json.loads(resp.data)
+        # check we have the same than in engine
+        self.assertListEqual(data["blocks"], self.engine.as_dict()["blocks"])
+        self.assertEqual(data["args"], ["in"])
+        self.assertListEqual(data["returns"], ["in", "middle", "out"])
+
+    def test_play_simple(self):
+        # it should be possible to play the full engine
+        # prepare query
+        rdata = {'in': 2}
+        rdata["options"] = {
+            'op1': [{
+                'name': 'mult_opt',
+            }]
+        }
+        json_data = json.dumps(rdata)
+        resp = self.app.get('api/play', data=json_data, content_type='application/json')
+        # load the results
+        resp_data = json.loads(resp.data)
+        # check it
+        self.assertTrue("results" in resp_data)
+        results = resp_data["results"]
+        self.assertTrue("out" in results)
+        self.assertEquals(len(results), 3) # in, middle, out
+        self.assertEquals(results["out"], 2*5*12)
+
+    def test_play_skip_op1(self):
+        # it should be possible to play the only the block op2
+        # prepare query
+        rdata = {'middle': 2}
+        rdata["options"] = {
+            'op1': []
+        }
+        json_data = json.dumps(rdata)
+        resp = self.app.get('api/play', data=json_data, content_type='application/json')
+        # load the results
+        resp_data = json.loads(resp.data)
+        # check it
+        self.assertTrue("results" in resp_data)
+        results = resp_data["results"]
+        self.assertTrue("out" in results)
+        self.assertEquals(len(results), 2) # middle, out
+        self.assertEquals(results["out"], 2*12)
+
+        #TODO: test error when wrong input
+
 
